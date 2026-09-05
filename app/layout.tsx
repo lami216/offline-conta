cc8f8d2f9cadacc9ee7b68d0202addadfd1c5efd
lami216 <iiyad21694@gmail.com>
2026-09-05 20:19:48 +0000

    Load authenticated locale layout corrections

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { APP_NAME } from "../lib/app-brand";
 import { LocaleProvider } from "./i18n/provider";
 import { direction, LOCALE_COOKIE, normalizeLocale } from "./i18n/locale";
 import "./globals.css";
+import "./locale-layout.css";
 
 export async function generateMetadata(): Promise<Metadata> {
  const locale=normalizeLocale((await cookies()).get(LOCALE_COOKIE)?.value);
